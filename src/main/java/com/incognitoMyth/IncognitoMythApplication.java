5b5bb372ed9941fd2fc761ac9fe49678fd984882
adarshjos <adarshjoseph97@gmail.com>
2020-06-30 14:39:58 +0530

    Initial Project Structure

---
+++ src/main/java/com/incognitoMyth/IncognitoMythApplication.java
@@ -0,0 +1,14 @@
+package com.incognitoMyth;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class IncognitoMythApplication {
+	public static void main(String[] args) {
+			SpringApplication.run(IncognitoMythApplication.class, args);
+		}
+	}
+
---
+++ src/test/java/com/incognitoMyth/IncognitoMythApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incognitoMyth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncognitoMythApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
