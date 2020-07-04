7bc7ea247e9d5b227bf1d19ab8c1deade812bc57
adarshjos <adarshjoseph97@gmail.com>
2020-07-04 15:13:24 +0530

    Service layer added

--- src/main/java/com/incognitoMyth/controller/IncognitoController.java
+++ src/main/java/com/incognitoMyth/controller/IncognitoController.java
@@ -4,6 +4,7 @@ package com.incognitoMyth.controller;
 import com.incognitoMyth.model.DTO.BrowserTabDTO;
 import com.incognitoMyth.model.entites.BrowserTabEntity;
 import com.incognitoMyth.repositories.BrowserTabRepository;
+import com.incognitoMyth.service.IncognitoService;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
@@ -19,71 +20,32 @@ import java.util.logging.Logger;
 public class IncognitoController {
     private static final Logger log=Logger.getLogger(IncognitoController.class.getName());
 
-    private final BrowserTabRepository browserTabRepository;
 
+    private final IncognitoService incognitoService;
     @Autowired
-    public IncognitoController(BrowserTabRepository browserTabRepository) {
-        this.browserTabRepository = browserTabRepository;
+    public IncognitoController(IncognitoService incognitoService) {
+        this.incognitoService=incognitoService;
     }
 
     @SubscribeMapping("/fp/initFP/{fingerPrint}")
     public BrowserTabDTO initFP(@DestinationVariable("fingerPrint") String transferData) throws Exception {
         log.log(Level.INFO,"FingerPrint Received':::"+transferData.toString());
-        BrowserTabDTO browserTabDTO=new BrowserTabDTO();
         JSONObject dataObj=new JSONObject(transferData);
-        if(!dataObj.has("fingerprint")){
-            throw new Exception("JSON key missing");
-        }
-        String fp=dataObj.getString("fingerprint");
-        try{
-            boolean fpExists=browserTabRepository.existsById(fp);
-            if(fpExists){
-                BrowserTabEntity browserTabEntity=browserTabRepository.findById(fp).get();
-                browserTabDTO.setFingerprint(browserTabEntity.getFingerprint());
-                browserTabDTO.setName(browserTabEntity.getName());
-                browserTabDTO.setStatus("FOUND");
-            }else{
-                browserTabDTO.setStatus("NOT FOUND");
-                browserTabDTO.setFingerprint(fp);
-            }
-
-        }catch(Exception e){
-            browserTabDTO.setStatus("NOT FOUND");
-            browserTabDTO.setFingerprint(fp);
-            log.log(Level.SEVERE,"Exception while DB fetching::");
-        }
-        return browserTabDTO;
+        return incognitoService.bootStrap(dataObj);
     }
 
-    @Transactional
+
     @MessageMapping("/insertFP")
-    //@SendTo("/topic/greetings")
     public void dbEntry(String data) throws Exception {
         log.log(Level.SEVERE,"Message:::"+data);
         JSONObject dbObj=new JSONObject(data);
-        if(dbObj.has("fingerprint")&&dbObj.has("name")){
-            BrowserTabEntity browserTabEntity=new BrowserTabEntity();
-            browserTabEntity.setName(dbObj.getString("name"));
-            browserTabEntity.setFingerprint(dbObj.getString("fingerprint"));
-            try{
-                browserTabRepository.save(browserTabEntity);
-            }catch(Exception e){
-                log.log(Level.SEVERE,"Exception Message ::"+e);
-            }
-
-        }
+        incognitoService.populateDB(dbObj);
     }
     @Transactional
     @MessageMapping("/forgetFP")
     public void forgetFP(String data) throws Exception {
         log.log(Level.INFO,"Forgetting FP:::"+data);
         JSONObject dbObj=new JSONObject(data);
-        if(dbObj.has("fingerprint")){
-            try{
-                browserTabRepository.deleteById(dbObj.getString("fingerprint"));
-            }catch(Exception e){
-                log.log(Level.SEVERE,"Exception Message ::"+e);
-            }
-        }
+        incognitoService.deleteEntry(dbObj);
     }
 }
---
+++ src/main/java/com/incognitoMyth/service/IncognitoService.java
@@ -0,0 +1,76 @@
+package com.incognitoMyth.service;
+
+import com.incognitoMyth.controller.IncognitoController;
+import com.incognitoMyth.model.DTO.BrowserTabDTO;
+import com.incognitoMyth.model.entites.BrowserTabEntity;
+import com.incognitoMyth.repositories.BrowserTabRepository;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@Service
+public class IncognitoService {
+    private static final Logger log=Logger.getLogger(IncognitoService.class.getName());
+    private final BrowserTabRepository browserTabRepository;
+    @Autowired
+    public IncognitoService(BrowserTabRepository browserTabRepository) {
+        this.browserTabRepository = browserTabRepository;
+    }
+
+
+    public BrowserTabDTO bootStrap(JSONObject dataObj) throws Exception {
+        if(!dataObj.has("fingerprint")){
+            throw new Exception("JSON key missing");
+        }
+        BrowserTabDTO browserTabDTO=new BrowserTabDTO();
+        String fp=dataObj.getString("fingerprint");
+        try{
+            boolean fpExists=browserTabRepository.existsById(fp);
+            if(fpExists){
+                BrowserTabEntity browserTabEntity=browserTabRepository.findById(fp).get();
+                browserTabDTO.setFingerprint(browserTabEntity.getFingerprint());
+                browserTabDTO.setName(browserTabEntity.getName());
+                browserTabDTO.setStatus("FOUND");
+            }else{
+                browserTabDTO.setStatus("NOT FOUND");
+                browserTabDTO.setFingerprint(fp);
+            }
+        }catch(Exception e){
+            browserTabDTO.setStatus("NOT FOUND");
+            browserTabDTO.setFingerprint(fp);
+            log.log(Level.SEVERE,"Exception while DB fetching::");
+        }
+        return browserTabDTO;
+    }
+
+
+    @Transactional
+    public void populateDB(JSONObject dbObj) throws Exception {
+        if(dbObj.has("fingerprint")&&dbObj.has("name")){
+            BrowserTabEntity browserTabEntity=new BrowserTabEntity();
+            browserTabEntity.setName(dbObj.getString("name"));
+            browserTabEntity.setFingerprint(dbObj.getString("fingerprint"));
+            try{
+                browserTabRepository.save(browserTabEntity);
+            }catch(Exception e){
+                log.log(Level.SEVERE,"Exception Message ::"+e);
+            }
+        }
+    }
+
+
+    public void deleteEntry(JSONObject dbObj) {
+        if(dbObj.has("fingerprint")){
+            try{
+                browserTabRepository.deleteById(dbObj.getString("fingerprint"));
+            }catch(Exception e){
+                log.log(Level.SEVERE,"Exception Message ::"+e);
+            }
+        }
+    }
+}
