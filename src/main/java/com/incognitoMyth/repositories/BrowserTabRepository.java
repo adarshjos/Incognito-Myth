e57f8bd87871e93270df4b45aa7e10cda574b039
adarshjos <adarshjoseph97@gmail.com>
2020-07-07 01:21:38 +0530

    (db_query) query construction

--- src/main/java/com/incognitoMyth/repositories/BrowserTabRepository.java
+++ src/main/java/com/incognitoMyth/repositories/BrowserTabRepository.java
@@ -4,7 +4,12 @@ import com.incognitoMyth.model.entites.BrowserTabEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface BrowserTabRepository extends JpaRepository<BrowserTabEntity,String> {
+    List<BrowserTabEntity> getBrowserTabEntitiesByFingerprintEquals(String fingerPrint);
+    void deleteBrowserTabEntityByFingerprintEquals(String fingerprint);
 
 }
